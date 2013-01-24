e650c4b941e4ae973e6a63effe8fbe126880d787
Manabu Ejima <jemiam@gmail.com>
2013-01-24 20:54:54 +0900

    check_status.sh: changed to check one node at one execution

--- setup.shinjuku/check_status.sh
+++ setup.shinjuku/check_status.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-dcmgr_services="
-admin
-auth
-proxy
-collector
-webui
-dcmgr
-metadata
-"
-#nwmongw
-#nsa
-#sta
-
-hva_services=(hva hva-worker)
-
-error=
+set -e
 
 function check(){
   local name=$1
@@ -33,24 +18,51 @@ function check(){
   fi
   echo success
 
-  for service in `eval echo \$\{${name}_services[@]\}`
+  local services= status= status_line= job_id=
+  for service in $(eval echo \$\{${name}_services[@]\})
   do
     echo -ne "vdc-${service}\t"
-    status=`ssh root@${ip} -C "initctl status vdc-${service}" 2>/dev/null`
-    status=`echo ${status} | awk '{print $2}' | sed -e 's/,//'`
-    echo ${status}
-    if [[ -z `echo $status | grep -o 'start/running'` ]]; then
+    status_line=$(ssh root@${ip} -C "initctl list | grep vdc-${service}" 2>/dev/null)
+    status=$(echo ${status_line} | awk '{print $2}' | sed -e 's/,//')
+    [[ ${status} =~ \(.*\) ]] && {
+      # service which started with ID
+      # ex) vdc-hva-worker (openvz64) start/running
+      job_id=$(echo ${status} | sed 's/[()]//g')
+      status=$(echo ${status_line} | awk '{print $3}' | sed -e 's/,//')
+    }
+
+    if [[ -z $(echo $status | grep -o 'start/running') ]]; then
       error=y
     fi
+    echo ${status}
   done
   echo 
 }
 
+dcmgr_services="
+admin
+auth
+proxy
+collector
+webui
+dcmgr
+metadata
+"
+#nwmongw
+#nsa
+#sta
 
-check dcmgr 10.0.2.15
-check hva 10.0.2.16
+hva_services=(hva hva-worker)
+
+error=
+
+name=$1
+ip=$2
+
+check ${name} ${ip}
 
 if [[ "${error}" = "y" ]]; then
+  echo "[ERROR] check status failed." >&2
   exit 1
 fi
-
+echo "check status was successful."
