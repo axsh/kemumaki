45e5c2166dd5d6ea832f3b94ca783e4966f37f04
Masahito Yoshida <masahito@axsh.net>
2013-05-17 21:35:08 +0900

    rpmbuild: fix archs -> distro_archs.

--- lib/setup-ci-env.sh
+++ lib/setup-ci-env.sh
@@ -17,7 +17,7 @@ function setup_chroot_dir() {
   distro="${distro_name}-${distro_ver}"
   distro_detail="${distro_name}-${distro_ver}.${distro_subver}"
 
-  for distro_arch in ${distro_archs}; do
+  for distro_arch in ${archs}; do
     [ -f ${distro_detail}_${distro_arch}.tar.gz ] || curl -R -O http://dlc.wakame.axsh.jp.s3.amazonaws.com/demo/rootfs-tree/${distro_detail}_${distro_arch}.tar.gz
     [ -d ${distro_detail}_${distro_arch}        ] || tar zxpf ${distro_detail}_${distro_arch}.tar.gz
     [ -d ${distro}_${distro_arch}               ] || mv ${distro_detail}_${distro_arch} ${distro}_${distro_arch}
