be34e79b35685bede1403a0112f94dc7d02c0abf
Masahito Yoshida <masahito@axsh.net>
2013-05-20 11:58:50 +0900

    change curl option.

--- lib/setup-ci-env.sh
+++ lib/setup-ci-env.sh
@@ -18,7 +18,7 @@ function setup_chroot_dir() {
   distro_detail="${distro_name}-${distro_ver}.${distro_subver}"
 
   for distro_arch in ${archs}; do
-    [ -f ${distro_detail}_${distro_arch}.tar.gz ] || curl -R -O http://dlc.wakame.axsh.jp.s3.amazonaws.com/demo/rootfs-tree/${distro_detail}_${distro_arch}.tar.gz
+    [ -f ${distro_detail}_${distro_arch}.tar.gz ] || curl -fkL -O http://dlc.wakame.axsh.jp.s3.amazonaws.com/demo/rootfs-tree/${distro_detail}_${distro_arch}.tar.gz
     [ -d ${distro_detail}_${distro_arch}        ] || tar zxpf ${distro_detail}_${distro_arch}.tar.gz
     [ -d ${distro}_${distro_arch}               ] || mv ${distro_detail}_${distro_arch} ${distro}_${distro_arch}
   done
