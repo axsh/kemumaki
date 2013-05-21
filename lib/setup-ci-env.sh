a72599743c4bb6f1dd7f2c829fb290f85286e7dc
Masahito Yoshida <masahito@axsh.net>
2013-05-21 15:22:27 +0900

    remove rootfs-tree tarball dependency.

--- lib/setup-ci-env.sh
+++ lib/setup-ci-env.sh
@@ -8,22 +8,18 @@ set -e
 function setup_chroot_dir() {
   distro_name="centos"
   distro_ver="6"
-  distro_subver="4"
-
-  distro_detail="${distro_name}-${distro_ver}.${distro_subver}"
 
   for distro_arch in ${archs}; do
-    base_dir=${rpmbuild_tmp_dir}/chroot/base
-    [ -d ${base_dir} ] || mkdir -p ${base_dir}
-    cd ${base_dir}
-
     distro_dir=${rpmbuild_tmp_dir}/chroot/base/${distro_name}-${distro_ver}_${distro_arch}
     chroot_dir=${rpmbuild_tmp_dir}/chroot/dest/${distro_name}-${distro_ver}_${distro_arch}
 
-    distro_targz_file=${distro_detail}_${distro_arch}.tar.gz
-    [ -f ${distro_targz_file}            ] || curl -fkL -O http://dlc.wakame.axsh.jp.s3.amazonaws.com/demo/rootfs-tree/${distro_targz_file}
-    [ -d ${distro_detail}_${distro_arch} ] || tar zxpf ${distro_targz_file}
-    [ -d ${distro_dir}                   ] || mv ${distro_detail}_${distro_arch} ${distro_dir}
+    ../vmbuilder/kvm/rhel/6/vmbuilder.sh \
+      --distro-name=${distro_name} \
+      --distro-ver=${distro_ver}   \
+      --distro-dir=${distro_dir}   \
+      --chroot-dir=                \
+      --hypervisor=null            \
+      --raw=${chroot_dir}.raw
 
     [[ -d "${chroot_dir}" ]] || mkdir -p ${chroot_dir}
     rsync -ax --delete ${distro_dir}/ ${chroot_dir}/
