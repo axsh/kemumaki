4234d9dd589b2eccd394f3c1b586ffbe8bed9ca9
Masahito Yoshida <masahito@axsh.net>
2013-05-21 11:56:36 +0900

    rpmbuild: update code layout.

--- lib/setup-ci-env.sh
+++ lib/setup-ci-env.sh
@@ -6,9 +6,6 @@ set -e
 . ./../config/rpmbuild.conf
 
 function setup_chroot_dir() {
-  [ -d ${rpmbuild_tmp_dir}/chroot/base ] || mkdir -p ${rpmbuild_tmp_dir}/chroot/base/
-  cd ${rpmbuild_tmp_dir}/chroot/base
-
   distro_name="centos"
   distro_relver="6"
   distro_subver="3"
@@ -18,6 +15,10 @@ function setup_chroot_dir() {
   distro_detail="${distro_name}-${distro_ver}.${distro_subver}"
 
   for distro_arch in ${archs}; do
+    base_dir=${rpmbuild_tmp_dir}/chroot/base
+    [ -d ${base_dir} ] || mkdir -p ${base_dir}
+    cd ${base_dir}
+
     [ -f ${distro_detail}_${distro_arch}.tar.gz ] || curl -fkL -O http://dlc.wakame.axsh.jp.s3.amazonaws.com/demo/rootfs-tree/${distro_detail}_${distro_arch}.tar.gz
     [ -d ${distro_detail}_${distro_arch}        ] || tar zxpf ${distro_detail}_${distro_arch}.tar.gz
     [ -d ${distro}_${distro_arch}               ] || mv ${distro_detail}_${distro_arch} ${distro}_${distro_arch}
