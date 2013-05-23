40ed7f7d8eee5678dc2a5f0e5897c1fbcc82c3f5
Masahito Yoshida <masahito@axsh.net>
2013-05-23 13:36:52 +0900

    remove current directory dependency.

--- lib/spot-build.sh
+++ lib/spot-build.sh
@@ -6,7 +6,7 @@
 #
 set -e
 
-. ./../config/rpmbuild.conf
+. $(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/../config/rpmbuild.conf
 
 vdc_dir=$1
 vdc_branch=${2:-master}
@@ -16,7 +16,7 @@ vdc_branch=${2:-master}
   exit 1
 }
 
-(cd .. &&  git submodule update --init)
+(cd ${abs_dirname} && git submodule update --init)
 
 [[ -d ${rpm_dir} ]] && rm -rf ${rpm_dir}
 
@@ -34,9 +34,9 @@ for arch in ${archs}; do
     distro_arch=${arch}
 
     time setarch ${arch} \
-     ../vmbuilder/kvm/rhel/6/vmbuilder.sh \
+     ${abs_dirname}/vmbuilder/kvm/rhel/6/vmbuilder.sh \
      --swapsize=0 \
-     --execscript=$(pwd)/xexecscript.sh  \
+     --execscript=${abs_dirname}/lib/xexecscript.sh  \
      --hypervisor=null \
      --distro-name=${distro_name} \
      --distro-ver=${distro_ver}   \
@@ -50,7 +50,7 @@ done
   createrepo .
 )
 
-./gen-index-html.sh > ${rpm_dir}/index.html
+${abs_dirname}/lib/gen-index-html.sh > ${rpm_dir}/index.html
 
 [[ -d ${yum_repository_dir}/${vdc_branch} ]] || mkdir -p ${yum_repository_dir}/${vdc_branch}
 release_id=$(cd ${vdc_dir} && rpmbuild/helpers/gen-release-id.sh)
