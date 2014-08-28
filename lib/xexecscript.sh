cdcf5ccdd6758617c84d687feaaba134a3845b82
Masahito Yoshida <masahito@axsh.net>
2014-08-28 14:14:44 +0900

    change baseurl from riken to dh.

--- lib/xexecscript.sh
+++ lib/xexecscript.sh
@@ -38,13 +38,13 @@ if [[ -f ${chroot_dir}/etc/yum/vars/releasever ]]; then
   cat <<-REPO > ${chroot_dir}/etc/yum.repos.d/CentOS-Base.repo
 	[base]
 	name=CentOS-\$releasever - Base
-	baseurl=http://ftp.riken.jp/Linux/centos/\$releasever/os/\$basearch/
+	baseurl=http://centos.data-hotel.net/pub/linux/centos/\$releasever/os/\$basearch/
 	gpgcheck=1
 	gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-${majorver}
 
 	[updates]
 	name=CentOS-\$releasever - Updates
-	baseurl=http://ftp.riken.jp/Linux/centos/\$releasever/updates/\$basearch/
+	baseurl=http://centos.data-hotel.net/pub/linux/centos/\$releasever/updates/\$basearch/
 	gpgcheck=1
 	gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-${majorver}
 	REPO
